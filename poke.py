f51084f1eb40bb131d0443c551099064e03f1fb2
Johnny Karhinen <johnny@nurrd.se>
2012-06-21 13:39:50 +0200

    Not so simple, but it schedules itself once an hour. Pretty nifty!

--- poke.py
+++ poke.py
@@ -1,10 +1,26 @@
 import heroku
 import requests
 import os
+import sched
+import time
 
-cloud = heroku.from_key(os.environ['HEROKU_API_KEY'])
 
-for app in cloud.apps:
-    domain = 'http://{app}.herokuapp.com'.format(app=app.name)
+def poke(domain):
     print 'Poking {domain}'.format(domain=domain)
     requests.get(domain)
+
+
+def poke_all(apps):
+    for app in apps:
+        poke('http://{app}.herokuapp.com'.format(app=app.name))
+
+
+def run(cloud, sch):
+    poke_all(cloud.apps)
+    sch.enter(3600, 1, run, (cloud, sch))
+
+if __name__ == '__main__':
+    cloud = heroku.from_key(os.environ['HEROKU_API_KEY'])
+    scheduler = sched.scheduler(time.time, time.sleep)
+    run(cloud, scheduler)
+    scheduler.run()
