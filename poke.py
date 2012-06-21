1e897fe2719089d971443c39719090e1cdf9406e
Johnny Karhinen <johnny@nurrd.se>
2012-06-21 13:17:20 +0200

    Even simpler.

--- poke.py
+++ poke.py
@@ -5,10 +5,6 @@ import os
 cloud = heroku.from_key(os.environ['HEROKU_API_KEY'])
 
 for app in cloud.apps:
-    try:
-        domain = 'http://' + app.domains[0].domain
-    except IndexError:  # if there are no custom domains
-        domain = 'http://{app}.herokuapp.com'.format(app=app.name)
-
+    domain = 'http://{app}.herokuapp.com'.format(app=app.name)
     print 'Poking {domain}'.format(domain=domain)
     requests.get(domain)
